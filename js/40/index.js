1f3223b4eacc1d78247439c8a52983baae750ae8
Dayasagar543 <dayasagar.543@gmail.com>
2024-12-16 14:52:31 +0530

    started the js and css both

---
+++ js/40/index.js
@@ -0,0 +1,18 @@
+var data = 21; // can be reassigned
+let name1 = "srikanth"; // reassignable
+const value = 3.14; //constant value
+console.log(data);
+console.log(name1);
+console.log(value);
+
+// function run() {
+//   var day = "thursday";
+//   console.log(day);
+// }
+
+const run = () => {
+  var day = "thursday";
+  console.log(day);
+};
+
+run();
