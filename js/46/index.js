1cc84cbe451bc9b1c437a5134e201da60f15ec87
Dayasagar543 <dayasagar.543@gmail.com>
2025-02-10 14:37:22 +0530

    adding the lines on temporal dead zone  block function scope global scope etc

--- js/46/index.js
+++ js/46/index.js
@@ -1,30 +1,68 @@
-//global scope avoid global declartion majorly
-var age = 15;
-let name = "sundharam";
-let last_name = "gyara";
+// //global scope avoid global declartion majorly
+// var age = 15;
+// let name = "sundharam";
+// let last_name = "gyara";
 
-console.log(age);
+// console.log(age);
+
+// {
+//   console.log(age);
+// }
+
+// if (true) {
+//   console.log(age);
+// }
+
+// for (let i = 0; i <= 3; i++) {
+//   console.log(age);
+// }
+
+// const flow=()=>{
+//     console.log(age)
+// }
+
+// flow()
+// function flow1(){
+//     console.log(age)
+// }
+
+// flow1()
+
+//function scoping
+function sayhi() {
+  var planet = "earth";
+  console.log("say hi", planet);
+}
+
+sayhi();
+
+// console.log(planet)
+
+//block scope
 
 {
+  var height = 5.4;
+  const age = 25;
+  let intrest = "music";
   console.log(age);
+  console.log(intrest);
 }
+console.log(height);
+// // console.log(age)
+// console.log(intrest)
 
-if (true) {
-  console.log(age);
-}
+// var  will over ride the  blockscope
 
-for (let i = 0; i <= 3; i++) {
-  console.log(age);
-}
+//temporal dead zone
 
+// console.log(marks)
+// console.log("raviender")
+// console.log("sunday")
 
-const flow=()=>{
-    console.log(age)
-}
+// the lines above this will be in tmeporal dead zone cause
+// marks cannot acces the value as its not hoiseted as var ,but declared
+// by let and const so the below will not run and the process or control
+// donot move further
 
-flow()
-function flow1(){
-    console.log(age)
-}
-
-flow1()
+const marks = 100;
+console.log(marks);
---
+++ js/46/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+console.log(marks)
+console.log("raviender")
+console.log("sunday")
+const marks=100;
+console.log(marks)
