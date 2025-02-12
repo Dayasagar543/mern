8fb5a598317e6c1e7365cb85b469b2249aa17955
Dayasagar543 <dayasagar.543@gmail.com>
2025-02-12 11:20:05 +0530

    adding the defualt objects file in the folder  48

---
+++ js/48/index.js
@@ -0,0 +1,39 @@
+//maths object that is defaultly available in the js
+console.log("runing");
+
+console.log(Math.pi);
+
+console.log(Math.max(21, 2, 3, 4, 5, 6, 4, 3));
+
+console.log(Math.min(234, 5, 6, 7, 3, 4, 2, 3, 4, 4, 33, 3));
+
+console.log(Math.floor(23.2));
+
+console.log(Math.ceil(2.2));
+
+console.log(Math.round(2.2));
+
+console.log(Math.abs(2.2));
+
+console.log(Math.abs(-2.2));
+
+console.log(Math.random());
+
+console.log(Math.abs(Math.random() * 10));
+
+console.log(Math.sqrt(4));
+
+console.log(Math.pow(4, 2));
+
+//date object
+console.log(Date());
+
+let curr_date = new Date();
+
+console.log(curr_date);
+
+console.log(curr_date.getMonth());
+
+console.log(curr_date.getFullYear());
+
+console.log(curr_date.getHours());
