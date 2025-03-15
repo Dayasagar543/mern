ebe9b40865459dc045d49f3da6b3a478c5dbcb91
Dayasagar543 <dayasagar.543@gmail.com>
2025-03-15 12:06:19 +0530

    added the basic div and the code of the counter in the app.jsx

--- React/60/react-project-folder60/src/App.jsx
+++ React/60/react-project-folder60/src/App.jsx
@@ -1,26 +1,29 @@
 import { useState } from "react";
+import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <div className="container">
         <h1>hi there manually created heading</h1>
         <div>{count}</div>
-        <button
-          onClick={(e) => {
-            setCount(count + 1);
-          }}
-        >
-          add
-        </button>
-        <button
-          onClick={(e) => {
-            setCount(count - 1);
-          }}
-        >
-          subtract
-        </button>
+        <div>
+          <button
+            onClick={(e) => {
+              setCount(count + 1);
+            }}
+          >
+            add
+          </button>
+          <button
+            onClick={(e) => {
+              setCount(count - 1);
+            }}
+          >
+            subtract
+          </button>
+        </div>
       </div>
     </>
   );
