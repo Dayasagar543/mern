f362cb3d57735d6599620d3ebe46b3c3c990de94
Dayasagar543 <dayasagar.543@gmail.com>
2025-04-16 11:21:42 +0530

    added the code in the js

--- js/59/index.js
+++ js/59/index.js
@@ -1,15 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault();
   //input and search
   let user_input = document.getElementById("user_input");
   let button = document.getElementById("Search");
 
   //piecharts
-  let stats_container = document.querySelector("stats_container");
+  let stats_container = document.querySelector(".stats_container");
   let easy_progress = document.querySelector(".easy_progress_item");
-  let medium_progress = document.querySelector(".medium_progress_item");
-  let hard_progress = document.querySelector(".hard_progress_item");
   let easy_progress_label = document.getElementById("easy_label");
+  let medium_progress = document.querySelector(".medium_progress_item");
   let medium_progress_label = document.getElementById("medium_label");
+  let hard_progress = document.querySelector(".hard_progress_item");
   let hard_progress_label = document.getElementById("hard_label");
 
   //cards container
@@ -31,9 +32,41 @@ document.addEventListener("DOMContentLoaded", () => {
     return isMatching;
   };
 
+  const Update_progress = (solved, total, label, circle) => {
+    const progressDegree = (solved / total) * 100;
+    circle.style.setProperty("--progress_degree", `${progressDegree}%`);
+    label.textContent = `${solved}/${total}`;
+  };
   const display_userdata = (Parseddata) => {
-    console.log(Parseddata)
-    // const totalHard_questions=Parseddata.
+    let data = Parseddata;
+    console.log(data);
+    let total_questions = Parseddata.totalQuestions;
+    let total_solved = Parseddata.totalSolved;
+    let total_easy = Parseddata.totalEasy;
+    let total_easy_solved = Parseddata.easySolved;
+    let total_medium = Parseddata.totalMedium;
+    let total_medium_solved = Parseddata.mediumSolved;
+    let total_hard = Parseddata.totalHard;
+    let total_hard_solved = Parseddata.hardSolved;
+
+    Update_progress(
+      total_easy_solved,
+      total_easy,
+      easy_progress_label,
+      easy_progress
+    );
+    Update_progress(
+      total_medium_solved,
+      total_medium,
+      medium_progress_label,
+      medium_progress
+    );
+    Update_progress(
+      total_hard_solved,
+      total_hard,
+      hard_progress_label,
+      hard_progress
+    );
   };
 
   const Fetch_data = async (user_name) => {
@@ -50,7 +83,7 @@ document.addEventListener("DOMContentLoaded", () => {
       //   console.log(data);
       display_userdata(Parseddata);
     } catch (error) {
-      stats_container.innerHTML = `<p>No data found !</p>`;
+      stats_container.innerHTML = `<p>${error.message}</p>`;
     } finally {
       button.textContent = "search";
       button.disabled = false;
