bae199ebbbb9f43c046f4efcf69a2e48ec7840ce
Dayasagar543 <dayasagar.543@gmail.com>
2025-04-26 11:03:57 +0530

    added the files of the servers and request methods

---
+++ back_end/79/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hellow world!");
+});
+
+// requests
+
+// get requests
+app.get("/home", (req, res) => {
+  res.send("hi there ");
+});
+
+app.put("/items/:id", (req, res) => {
+  res.send("got a put request");
+});
+
+app.post("/items/:id", (req, res) => {
+  res.send("got a post request");
+});
+
+app.delete("/items/:id", (req, res) => {
+  res.send("got a delete request");
+});
+
+app.listen(port, () => {
+  console.log(`dev  app listening on port ${port}`);
+});
