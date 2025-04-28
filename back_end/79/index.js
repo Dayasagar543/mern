03c029cd3a6c04eb9c8598ae6bcd9562683f330a
Dayasagar543 <dayasagar.543@gmail.com>
2025-04-28 09:15:32 +0530

    made the changes in the index.js file

--- back_end/79/index.js
+++ back_end/79/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// app.get("/", (req, res) => {
+//   res.send("hellow world!");
+// });
 app.get("/", (req, res) => {
-  res.send("hellow world!");
+  res.sendFile('./index.html')
 });
 
 // requests
