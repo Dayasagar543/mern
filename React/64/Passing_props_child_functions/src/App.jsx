5bd7b017da2a9983b227b4952dabae3f8724184b
Dayasagar543 <dayasagar.543@gmail.com>
2025-09-10 12:56:23 +0530

    added the styling in the code

--- React/64/Passing_props_child_functions/src/App.jsx
+++ React/64/Passing_props_child_functions/src/App.jsx
@@ -9,14 +9,18 @@ function App() {
   };
 
   return (
-    <div className="color_bg">
-      <h1>HI THERE YESUVA LOVES YOU</h1>
-      <p>This is to inform you that you are loved</p>
-      <Card text="click me" Count_increment={Count_increment}>
-        <h1>This is from card</h1>
-        <p>we are going to make things work</p>
-        <h2>{count}</h2>
-      </Card>
+    <div className="container">
+      <header className="header">
+        <h1>HI THERE YESUVA LOVES YOU</h1>
+        <p>This is to inform you that you are loved</p>
+      </header>
+      <main className="main">
+        <Card text="click me" Count_increment={Count_increment}>
+          <h1>This is from card</h1>
+          <p>we are going to make things work</p>
+          <h2>{count}</h2>
+        </Card>
+      </main>
     </div>
   );
 }
